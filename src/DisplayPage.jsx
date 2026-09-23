61525075985bc357bb73cca9f8eeed2d8509af14
bryanjo28 <48087968+bryanjo28@users.noreply.github.com>
2026-09-23 15:49:55 +0700

    updated

--- src/DisplayPage.jsx
+++ src/DisplayPage.jsx
@@ -300,6 +300,7 @@ export default function DisplayPage() {
               }}
               controls={false}
             />
+            <div className="previewVideoTitle">{displayTitle}</div>
           </div>
         </div>
       </div>
